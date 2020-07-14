06d7271c0a1adf104640c5000a2467f8bfab4b7d
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2020-07-13 22:29:34 -0300

    Added icon config

--- pwkbridge.cpp
+++ pwkbridge.cpp
@@ -12,9 +12,14 @@ int main(int argc, char *argv[])
 
 	// get config
 	QSettings my_settings("config.ini", QSettings::IniFormat);
-	QString config_url = my_settings.value("URL", "").toString();
+	QString config_url = my_settings.value("url", "").toString();
 	bool config_debug = my_settings.value("debug", "").toBool();
+	QString config_icon = my_settings.value("icon", "").toString();
 
+	// set icon
+	app.setWindowIcon(QIcon(config_icon));
+
+	// run webkit
 	JSBridge *jsBridge_ = new JSBridge(config_debug);
 	jsBridge_->openUrl(config_url);
 	
